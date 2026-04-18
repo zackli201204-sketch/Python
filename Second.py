1f257ddd15eced3c2ae915cb466210f5b4275d23
zackli201204-sketch <zackli201204@gmail.com>
2026-04-18 20:14:23 +1000

    Add files via upload

---
+++ Second.py
@@ -0,0 +1,33 @@
+# print ("Welcome to the World of Programming!")
+# print ("683652")
+# print ("Hello Python \n")
+# print ("Hello" , "4532")
+# print ("Welcome to ", end= "The End")
+
+# y = 8
+# x = "Humans"
+
+# print (y)
+# print (x)
+
+# pyt = "Python"
+# print (pyt)
+
+# name = input ("enter your name")
+
+# print ("\nHello" , name, "\nWelcome to Python")
+
+# print ("What is your name")
+# name = input ("Enter Your Name")
+# print ("\nHello" , name, "\nHow old are you?")
+
+# age = input ("Enter your age")
+# print ("\nThat is very old , \nWhich continent do you live in?")
+
+# place = input ("Enter a continent")
+# print ("\nHeard it is very nice there ,you must be very lucky !")
+
+# import keyword
+
+# print ("Python keywords are . . . \n")
+# print (keyword.kwlist)

91707010bce11814f7b4f6728e7b85c8024cf95e
zackli201204-sketch <zackli201204@gmail.com>
2026-05-05 20:30:38 +1000

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,27 @@
+a = 10
+b = 12
+c = 12
+
+print(not (a == b))
+
+print(not (a == c))
+
+a = "python"
+b = "coding"
+
+if not (a == b):
+    print(a, 'and', b, 'are different.')
+
+a = 4
+a = 5
+
+if not ((a == 1) == (b == 5)):
+    print("Hello")
+
+a = int(input("Enter a Number: "))
+
+if not (a % 2 == 0):
+    print(a, "is an odd number.")
+
+if (a % 2 == 0):
+    print(a, "is an even number.")
